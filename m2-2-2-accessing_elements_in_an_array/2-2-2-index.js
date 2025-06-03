d9d92b5a9dfd1196f2d54ddc13e9d29c5f798be1
Chris Galentinemail <chrisgalentin.bloomtech@gmail.com>
2025-06-03 17:37:57 -0400

    added more from class

---
+++ m2-2-2-accessing_elements_in_an_array/2-2-2-index.js
@@ -0,0 +1,6 @@
+ /*const veggies = ['carrot', 'corn', 'potato'];
+    
+    const first = veggies[2];
+    console.log(first);
+
+    
---
+++ m3-1-1-using_the_for_loop/for-loop.js
@@ -0,0 +1,79 @@
+
+/*for (let i = 1; i <= 10; i++) {
+      console.log(`${i} Mississippi`)
+    }
+
+ 
+for (let i = 1; i <= 5; i++) {
+    console.log("Count is:", i);
+
+}
+
+const fruits =  ['apple', 'bannana', 'cherry']
+
+for (let i = 0; i < fruits.length; i++) {
+
+    console.log('fruits at index:', i,'is', fruits[i]);
+    console.log(fruits);
+    con
+}
+    
+
+for (let i = 2; i <= 10; i+= 2)
+        console.log("even numbers", i);
+
+let count = 10;
+while (count > 0) {
+    console.log("countdown:", count);
+    count--;
+};
+*/
+
+/*let userInput = "";
+
+const secretWord = "hello";
+
+while(userInput !== secretWord) {
+    userInput = prompt("Guess that word");
+
+}
+
+console.log("You guessed it");*/
+
+/* let number = 0;
+ while (number !== 10) {
+    number = Math.floor(Math.random() *10) +1
+    console.log("Rolled:", number)
+
+ }*/
+/*
+let password = "";
+while (password !== "password123") {
+    password = prompt("enter the password")
+}*/
+
+/*const snackMenu = ["doritos", "stringcheese", "pretzels", "goldfish"];
+const chosenSnack = [];
+
+let snackCounts = 0;
+
+while (snackCounts <= 3) {
+    let userSnack = prompt (
+        `Pick a snack! Choices are: ${snackMenu.join(",")}`
+    );
+
+    if (snackMenu.includes(userSnack)) {
+        console.log(`${userSnack} added to your order`);
+        chosenSnack.push(userSnack);
+        snackCounts++;
+        }
+        else {
+            console.log(`"${userSnack}" is not available. Try again`);
+        }
+    }
+    console.log("here is what you ordered:");
+    for (let i = 0; i < chosenSnack.length; i++) {
+        console.log(`Snack ${1 + 1}: ${chosenSnack[i]}`);
+    }*/
+
+        
